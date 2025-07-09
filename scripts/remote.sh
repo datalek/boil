5365647d234d4476dd8fab764cf73cfe2fa2f506
rakhov <ferlosalem@gmail.com>
2025-07-09 22:04:43 +0200

    Change remote script to avoid changing working dir
    Before this commit, the script changed to a temporary directory to download
    and extract files. After this commit, the script uses absolute paths to work
    with files in the temp directory without changing the current working
    directory.

--- scripts/remote.sh
+++ scripts/remote.sh
@@ -3,12 +3,11 @@
 set -e
 TEMP=$(mktemp -d)
 trap "rm -rf $TEMP" EXIT
-cd "$TEMP"
 
-# Downloading
-curl -sL https://github.com/datalek/boil/releases/latest/download/boil-node-22.tgz -o archive.tgz
-
+# Download and extract in temp (without changing working directory)
+curl -sL https://github.com/datalek/boil/releases/latest/download/boil-node-22.tgz -o "$TEMP/archive.tgz"
 # Extracting
-tar -xzf archive.tgz
+tar -xzf "$TEMP/archive.tgz" -C "$TEMP"
+
 # Redirect stdin to tty for interactive input
-npx node dist/boil.cjs "$@" < /dev/tty
+npx node "$TEMP/dist/boil.cjs" "$@" < /dev/tty
